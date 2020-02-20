9f527e1b8d61bd0f7ccc6ddde24fe86314765c4b
root <root@LAPTOP-R0QR6O7K.localdomain>
2020-02-20 14:28:59 +0100

    updated timer module

--- source/timer.c
+++ source/timer.c
@@ -3,10 +3,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#define TIME_OPEN 3
-
-//int const TIME_OPEN = 3; //dette vs #define TIME_OPEN 3
-
 time_t time_get_close() {
     return time(NULL) + TIME_OPEN;
 }
--- source/timer.h
+++ source/timer.h
@@ -1,6 +1,8 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#define TIME_OPEN 3
+
 time_t time_get_close();
 
 time_t time_get_current();
