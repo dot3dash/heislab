7417b693b67772831565db386a49ea2862ede8a4
jonska <jonska@stud.ntnu.no>
2020-02-23 17:25:58 +0100

    Fixed jerking to floor bug
    Signed-off-by: jonska <jonska@stud.ntnu.no>

--- source/fsm.h
+++ source/fsm.h
@@ -1,8 +1,14 @@
+/**
+ * @file
+ * @brief A Finite State Machine to control the elevator.
+ */
 #ifndef FSM_H
 #define FSM_H
 
-// #define TRUE 0, FALSE !(TRUE)?
 
+/**
+ * @brief The state types used by @c elevator_run().
+ */
 typedef enum {
     MOVING,
     IDLE,
@@ -10,6 +16,9 @@ typedef enum {
     STOPPED
 } ElevatorState;
 
+/**
+ *
+ */
 int elevator_initialize();
 
 void elevator_run(); 
